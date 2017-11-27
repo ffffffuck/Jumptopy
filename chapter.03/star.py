43a507dc79ee9a341b9266218ce8faf82df0a1cb
ffffffuck <chshone88@gmail.com>
2017-11-27 21:27:29 +0900

    개어려워

--- chapter.03/star.py
+++ chapter.03/star.py
@@ -1,16 +1,13 @@
-
-i = int(input("홀수를 입력하세요:"))
-
 while True:
-    i+=1
-    if i/2 == 0 : continue
-    print("짝수를 입력")
+    a = int(input("홀수를 입력하세요:"))
+    if a == 0:
+        print("마름모 연습프로그램을 이용해주셔서 감사합니다.")
+    if a % 2 == 0: continue
+    elif a > 0 :
+        for i in range(1,a+1):
+            for j in range(1,(a+1)-i):
+                print(' ', end='')
+            for j in range(1,i*2,1):
+                print('*', end='')
 
-    print(i*"*"-"*")
-    break
-
-
-
-
-
-
+            print()
