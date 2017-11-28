4d221e329e7c32ba8fe05724008453c51128109d
ffffffuck <chshone88@gmail.com>
2017-11-28 21:32:01 +0900

    개어려워...

--- chapter.03/star.py
+++ chapter.03/star.py
@@ -1,17 +1,20 @@
 while True:
-
-    a = int(input("홀수를 입력하세요(0<-프로그램종료):"))
-    if a % 2 == 0:
-        print("홀수를 입력하세요:")
+    a = int(input("홀수를 입력하세요.\n(0<-프로그램종료):"))
+    if a % 2 == 0 and a != 0:
+        print("올바른 수를 입력해주세요.")
         continue
-
+    if a == 0:
+        print("마름모 연습프로그램을 이용해 주셔서 감사합니다.")
+        break
     for i in range(1,(a+1)):
         for j in range(1,(a+1)-i):
             print(' ',end='')
         for i in range(1,(2*i)):
             print('*', end='')
-
         print()
-    if a == 0 :
-        print("마름모 연습프로그램을 이용해 주셔서 감사합니다.")
-        break
+    for q in range(1,a):
+        for w in range(1,q+1):
+            print(' ', end='')
+        for e in range(1,(2*a)-(2*q)):
+            print('*', end='')
+        print()
