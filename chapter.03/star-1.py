5a4f6ffb9a8d18c585e4537b30096ebf2281418e
ffffffuck <chshone88@gmail.com>
2017-11-29 19:57:06 +0900

    개어려워....

--- chapter.03/star-1.py
+++ chapter.03/star-1.py
@@ -6,9 +6,44 @@ while True:
         continue
     if a == 0:
         print("삼각형 연습 프로그램을 종료합니다. 감사합니다.")
-    for i in range(1,a+1):
-        for j in range(1,a+1-i):
-            print(' ', end='')
-        for j in range(1,2*i):
-            print('*', end='')
+
+    for i in range(1,2):
+        for i in range(1,2):
+            print(' ',end='')
+        for i in range(1,2):
+            print('-'*a,end='')
         print()
+
+    for i in range(1,a+2):
+        if i%2 ==1: continue
+        for j in range(1,2):
+            print('|',end='')
+        for j in range(0,(a+2-i)//2):
+            print(' ', end='')
+        for j in range(1,i):
+            print('*', end='')
+        for j in range(0, (a + 2 - i) // 2):
+            print(' ', end='')
+        for j in range(1,2):
+            print('|',end='')
+        print()
+    for i in range(1, a):
+        if i % 2 == 1: continue
+        for j in range(1,2):
+            print('|',end='')
+        for j in range(1, (i + 2) // 2):
+            print(' ', end='')
+        for j in range((2*(a-i))//2):
+            print('*', end='')
+        for j in range(1, (i + 2) // 2):
+            print(' ', end='')
+        for j in range(1, 2):
+            print('|', end='')
+        print()
+    for i in range(1, 2):
+        for i in range(1, 2):
+            print(' ', end='')
+        for i in range(1, 2):
+            print('-' * a, end='')
+        print()
+
