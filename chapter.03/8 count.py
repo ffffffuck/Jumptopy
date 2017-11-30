cbd87e2a0d35ab68346fc3901a180662be7c12a2
USER6-PC <you@example.com>
2017-11-30 17:27:47 +0900

    ㅇㅇ

---
+++ chapter.03/8 count.py
@@ -0,0 +1,13 @@
+s=[]
+for j in range(1,10001):
+    s.append(j)
+b=set(s)
+b=list(b)
+c=str(b)
+n=[i[0] for i in c ]
+print(n.count('8'))
+
+
+
+
+
---
+++ multiples of 3 and 5.py
@@ -0,0 +1,11 @@
+sumA=0
+sumB=0
+sumC=0
+for a in range(3,1000,3):
+        sumA=sumA+a
+for b in range(5,1000,5):
+        sumB=sumB+b
+for c in range(15,1000,15):
+        sumC=sumC+c
+print(sumA+sumB-sumC)
+
