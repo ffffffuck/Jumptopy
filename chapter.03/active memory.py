08a08785a37c213ee74c12c5f0a9e2c940ffc60d
ffffffuck <chshone88@gmail.com>
2017-12-03 18:25:34 +0900

    개어려워....

--- chapter.03/active memory.py
+++ chapter.03/active memory.py
@@ -1,9 +1,9 @@
 while True:
-        a= int(input("입력받을 갯수를 정하세요:"))
-        b=[]
-        for i in range(a):
-            i = int(input("정수를 입력하세요:"))
-            b.append(i)
-        print("합:", sum(b))
-        print("평균:", sum(b)//a)
-        del a,b,i
+    a= int(input("입력받을 갯수를 정하세요:"))
+    b=[]
+    for i in range(a):
+        i = int(input("정수를 입력하세요:"))
+        b.append(i)
+    print("합:", sum(b))
+    print("평균:", sum(b)//a)
+    del a,b,i
