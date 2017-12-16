c3b0e5da256072d3e8d29e6f493e30208fd26e97
chshone88@gmail.com <chshone88@gmail.com>
2017-12-17 01:36:13 +0900

    aa

--- chapter.06/12.15 prac2.py
+++ chapter.06/12.15 prac2.py
@@ -8,6 +8,7 @@ print("%s의 배수는 %s 입니다." %(int(n[2]),','.join([str(i) for i in e]))
 print("%s와 %s의 배수는 %s 입니다."%(int(n[1]),int(n[2]),','.join([str(i) for i in list(set(d+e))])))
 print("따라서 0부터 %s이하의 범위에서 %s와 %s의 배수의 총합은 %s 입니다."%(int(n[0]),int(n[1]),int(n[2]), sum(list(set(d+e)))))
 
+
 # n= input('범위, 첫번째수,두번째수를 입력하세요.(종료:프로그램 종료):').split()
 # d=[]
 # e=[]
